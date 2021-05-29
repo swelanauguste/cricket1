3f595d36fffca47b10016c911f7b975246846ffe
Swelan R. Auguste <swelanauguste@gmail.com>
2021-05-29 01:17:28 -0400

    Update urls.py

--- apps/cf/urls.py
+++ apps/cf/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('star/admins/', admin.site.urls),
     path('', include('users.urls', namespace='users')),
     path('inventory', include('inventory.urls', namespace='inventory')),
     path('accounts/', include('allauth.urls')),
